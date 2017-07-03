5ad4700b2d5fd7ede9301233ab3caab335e12c4f
Timothy Lui <timothy.lui@eatentechnologies.com>
2017-07-03 16:16:26 +0100

    make gestures only 1/4 of the screen width

--- src/views/CardStack.js
+++ src/views/CardStack.js
@@ -93,7 +93,7 @@ const RESPOND_THRESHOLD = 20;
 /**
  * The distance of touch start from the edge of the screen where the gesture will be recognized
  */
-const GESTURE_RESPONSE_DISTANCE_HORIZONTAL = window.width;
+const GESTURE_RESPONSE_DISTANCE_HORIZONTAL = window.width /4 ;
 const GESTURE_RESPONSE_DISTANCE_VERTICAL = 135;
 
 const animatedSubscribeValue = (animatedValue: Animated.Value) => {
